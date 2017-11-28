13751e3708c93f8ac997f1ee64939f48b0fea201
Jordan Petridis <jordanpetridis@protonmail.com>
2017-11-28 10:27:43 +0200

    Tweak gresource initialization.

--- hammond-gtk/src/static_resource.rs
+++ hammond-gtk/src/static_resource.rs
@@ -6,11 +6,8 @@ pub fn init() -> Result<(), Error> {
     let res_bytes = include_bytes!("../resources/resources.gresource");
 
     // Create Resource it will live as long the value lives.
-    // TODO: change it into Bytes::From_static once the fix lands
-    // https://bugzilla.gnome.org/show_bug.cgi?id=790030
-    let gbytes = Bytes::from(&res_bytes.as_ref());
+    let gbytes = Bytes::from_static(res_bytes.as_ref());
     let resource = Resource::new_from_data(&gbytes)?;
-    // let resource = Resource::new_from_data(&res_bytes.as_ref().into())?;
 
     // Register the resource so It wont be dropped and will continue to live in memory.
     resources_register(&resource);
