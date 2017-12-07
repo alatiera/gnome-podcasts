9431fb827f0556a3ab8f23e6203d72ecd5fabad6
Jordan Petridis <jordanpetridis@protonmail.com>
2017-12-07 08:42:36 +0200

    Bug fix.

--- hammond-gtk/src/widgets/podcast.rs
+++ hammond-gtk/src/widgets/podcast.rs
@@ -108,8 +108,8 @@ fn on_unsub_button_clicked(stack: &gtk::Stack, pd: &Podcast, unsub_button: &gtk:
             }
         };
     }
-    content::show_podcasts(stack);
     content::update_podcasts(stack);
+    content::show_podcasts(stack);
 }
 
 fn on_played_button_clicked(stack: &gtk::Stack, pd: &Podcast) {
