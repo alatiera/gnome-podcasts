5cd3dff1d4aa483c187a2d151284bc668db6c40b
Jordan Petridis <jordanpetridis@protonmail.com>
2018-02-05 20:41:45 +0200

    Downloader: Switch to returning Downloader::Error instead of failure::Error.

--- hammond-downloader/src/errors.rs
+++ hammond-downloader/src/errors.rs
@@ -1,13 +1,35 @@
-use hammond_data;
+use hammond_data::errors::DataError;
 use reqwest;
 use std::io;
 
 #[derive(Fail, Debug)]
-pub enum DownloaderError {
+pub enum DownloadError {
     #[fail(display = "Reqwest error: {}", _0)]
-    RequestError(reqwest::Error),
+    RequestError(#[cause] reqwest::Error),
     #[fail(display = "Data error: {}", _0)]
-    DataError(hammond_data::errors::DataError),
+    DataError(#[cause] DataError),
     #[fail(display = "Io error: {}", _0)]
-    IoError(io::Error),
+    IoError(#[cause] io::Error),
+    #[fail(display = "The Download was cancelled")]
+    DownloadCancelled,
+    #[fail(display = "Unexpected server response: {}", _0)]
+    UnexpectedResponse(reqwest::StatusCode),
+}
+
+impl From<reqwest::Error> for DownloadError {
+    fn from(err: reqwest::Error) -> Self {
+        DownloadError::RequestError(err)
+    }
+}
+
+impl From<io::Error> for DownloadError {
+    fn from(err: io::Error) -> Self {
+        DownloadError::IoError(err)
+    }
+}
+
+impl From<DataError> for DownloadError {
+    fn from(err: DataError) -> Self {
+        DownloadError::DataError(err)
+    }
 }
--- hammond-downloader/src/lib.rs
+++ hammond-downloader/src/lib.rs
@@ -1,7 +1,6 @@
 #![recursion_limit = "1024"]
 #![deny(unused_extern_crates, unused)]
 
-#[macro_use]
 extern crate failure;
 #[macro_use]
 extern crate failure_derive;
