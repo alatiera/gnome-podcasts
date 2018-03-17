f182d441d1d481165e26e9c6bc57a6319e99f42d
Rowan Lewis <stoatally@gmail.com>
2018-03-18 00:29:52 +0100

    Allow the Copy trait to do the work for us.

--- hammond-gtk/src/utils.rs
+++ hammond-gtk/src/utils.rs
@@ -42,13 +42,13 @@ pub fn get_refresh_interval(settings: &Settings) -> Duration {
     let time = settings.get_int("refresh-interval-time") as i64;
     let period = settings.get_string("refresh-interval-period").unwrap();
 
-    time_period_to_duration(&time, period.as_str())
+    time_period_to_duration(time, period.as_str())
 }
 
 pub fn get_cleanup_date(settings: &Settings) -> DateTime<Utc> {
     let time = settings.get_int("cleanup-age-time") as i64;
     let period = settings.get_string("cleanup-age-period").unwrap();
-    let duration = time_period_to_duration(&time, period.as_str());
+    let duration = time_period_to_duration(time, period.as_str());
 
     Utc::now() - duration
 }
@@ -164,13 +164,13 @@ fn lookup_id(id: u32) -> Result<String, Error> {
     Ok(feedurl.into())
 }
 
-pub fn time_period_to_duration(time: &i64, period: &str) -> Duration {
+pub fn time_period_to_duration(time: i64, period: &str) -> Duration {
     match period {
-        "weeks" => Duration::weeks(time.clone()),
-        "days" => Duration::days(time.clone()),
-        "hours" => Duration::hours(time.clone()),
-        "minutes" => Duration::minutes(time.clone()),
-        _ => Duration::seconds(time.clone()),
+        "weeks" => Duration::weeks(time),
+        "days" => Duration::days(time),
+        "hours" => Duration::hours(time),
+        "minutes" => Duration::minutes(time),
+        _ => Duration::seconds(time),
     }
 }
 
@@ -188,17 +188,14 @@ mod tests {
         let hour = 3600 * time;
         let minute = 60 * time;
 
-        assert_eq!(week, time_period_to_duration(&time, "weeks").num_seconds());
-        assert_eq!(day, time_period_to_duration(&time, "days").num_seconds());
-        assert_eq!(hour, time_period_to_duration(&time, "hours").num_seconds());
+        assert_eq!(week, time_period_to_duration(time, "weeks").num_seconds());
+        assert_eq!(day, time_period_to_duration(time, "days").num_seconds());
+        assert_eq!(hour, time_period_to_duration(time, "hours").num_seconds());
         assert_eq!(
             minute,
-            time_period_to_duration(&time, "minutes").num_seconds()
+            time_period_to_duration(time, "minutes").num_seconds()
         );
-        assert_eq!(
-            time,
-            time_period_to_duration(&time, "seconds").num_seconds()
-        );
+        assert_eq!(time, time_period_to_duration(time, "seconds").num_seconds());
     }
 
     #[test]
