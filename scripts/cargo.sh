b3d45384e1ac57dee2de41e0f376a567229c05eb
Jordan Petridis <jordanpetridis@protonmail.com>
2018-05-31 14:44:32 +0300

    meson: Add debug build.

--- scripts/cargo.sh
+++ scripts/cargo.sh
@@ -3,4 +3,11 @@
 export CARGO_HOME=$1/target/cargo-home
 export RUSTFLAGS="--cfg rayon_unstable"
 
-cargo build --release -p hammond-gtk && cp $1/target/release/hammond-gtk $2
+if [[ $DEBUG = true ]]
+then
+    echo "DEBUG MODE"
+    cargo build -p hammond-gtk && cp $1/target/debug/hammond-gtk $2
+else
+    echo "RELEASE MODE"
+    cargo build --release -p hammond-gtk && cp $1/target/release/hammond-gtk $2
+fi 
