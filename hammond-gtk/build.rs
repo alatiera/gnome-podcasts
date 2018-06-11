bbabc6f5e98deea9fa1dd659bdfcc4afc9ece639
Jordan Petridis <jordanpetridis@protonmail.com>
2018-06-11 14:23:29 +0300

    h-gtk: Add resources/ to the cargo check list.

--- hammond-gtk/build.rs
+++ hammond-gtk/build.rs
@@ -1,6 +1,10 @@
 use std::process::Command;
 
 fn main() {
+    // Rerun the build script when files in the resources folder are changed.
+    println!("cargo:rerun-if-changed=resources");
+    println!("cargo:rerun-if-changed=resources/*");
+
     Command::new("glib-compile-resources")
         .args(&["--generate", "resources.xml"])
         .current_dir("resources")
