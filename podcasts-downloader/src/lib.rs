5b77bb4649f57ea3d02b148ada49e47cee59a177
Jordan Petridis <jpetridis@gnome.org>
2018-11-04 05:08:58 +0200

    podcasts-downloader: Add per file license annotations

--- podcasts-downloader/src/lib.rs
+++ podcasts-downloader/src/lib.rs
@@ -1,3 +1,23 @@
+// lib.rs
+//
+// Copyright 2017 Jordan Petridis <jpetridis@gnome.org>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+
 #![recursion_limit = "1024"]
 #![allow(unknown_lints)]
 #![cfg_attr(feature = "cargo-clippy", allow(blacklisted_name, option_map_unit_fn))]
