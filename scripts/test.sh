53e9db2f42bf4b6ad593e20aed708bd9f306724c
Jordan Petridis <jpetridis@gnome.org>
2019-04-06 22:14:12 +0300

    scripts/test.sh: export the rust binaries iniside flatpak environments
    This allows the script to be able to be run directly from GNOME Builder's
    build terminals.

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,15 @@ set -o errexit
 set -o pipefail
 
 export CARGO_TARGET_DIR="$1/target/"
+
+# If this is run inside a flatpak envrironment, append the export the rustc
+# sdk-extension binaries to the path
+if [ -n "/.flatpak-info" ]
+then
+    export PATH="$PATH:/usr/lib/sdk/rust-stable/bin"
+    export CARGO_TARGET_DIR="$BUILDDIR/target/"
+fi
+
 export CARGO_HOME="$CARGO_TARGET_DIR/cargo-home"
 
 cargo test -j 1 -- --test-threads=1 --nocapture
