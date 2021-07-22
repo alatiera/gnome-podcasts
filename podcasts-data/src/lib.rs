2bd68fa7b24626dbb5f68bcbc4b8494284c84364
Jordan Petridis <jordan@centricular.com>
2021-07-22 03:55:19 +0300

    data: update user agent string

--- podcasts-data/src/lib.rs
+++ podcasts-data/src/lib.rs
@@ -102,7 +102,7 @@ pub use crate::models::{Episode, EpisodeWidgetModel, Show, ShowCoverModel, Sourc
 // Keep this in sync with Tor-browser releases
 /// The user-agent to be used for all the requests.
 /// It originates from the Tor-browser UA.
-pub const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 6.1; rv:60.0) Gecko/20100101 Firefox/60.0";
+pub const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0";
 
 /// [XDG Base Directory](https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html) Paths.
 #[allow(missing_debug_implementations)]
