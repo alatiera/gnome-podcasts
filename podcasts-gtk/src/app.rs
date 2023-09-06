39769f24659da66273054bc46b560132dedaa445
Jordan Petridis <jpetridis@gnome.org>
2023-09-06 16:13:34 +0000

    Revert "app: use set_prgname"
    With GTK 4 set_prgname does not need to be set, and it's
    suggested to remove it in th migration guide. Whatever
    issue this was fixing for phosh is a bug somewhere else and
    should be investigated further.
    
    This reverts commit d0f66ea3b6b656d4b858ae9af55219b972dcdbb0

--- podcasts-gtk/src/app.rs
+++ podcasts-gtk/src/app.rs
@@ -527,8 +527,6 @@ impl PdApplication {
 
         let application = Self::new();
 
-        glib::set_prgname(Some(APP_ID));
-
         // Weird magic I copy-pasted that sets the Application Name in the Shell.
         glib::set_application_name(&i18n("Podcasts"));
         gtk::Window::set_default_icon_name(APP_ID);
