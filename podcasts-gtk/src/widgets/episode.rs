5aa055720d1803f765348d8cc4068824b2c9d841
nee <nee-git@patchouli.garden>
2025-01-22 02:18:00 +0100

    episode-menu: don't display the arrow in the home-page popover.
    After design feedback the pointy arrow/triangle should be hidden from
    the menu. The menu is visible on rightclicking episodes.
    
    Closes #373

--- podcasts-gtk/src/widgets/episode.rs
+++ podcasts-gtk/src/widgets/episode.rs
@@ -411,6 +411,7 @@ impl EpisodeWidgetPriv {
                 popover.set_parent(&*this.obj());
                 popover.insert_action_group("episode", Some(&menu.group));
                 popover.set_pointing_to(Some(&gtk::gdk::Rectangle::new(x as i32, y as i32, 1, 1)));
+                popover.set_has_arrow(false);
                 popover.popup();
             }
         );
