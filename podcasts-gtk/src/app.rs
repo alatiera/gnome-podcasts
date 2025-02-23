f74b9c85beb06d3e7273a3189c026da312b67138
Maximiliano Sandoval <msandova@gnome.org>
2025-02-23 08:34:37 +0100

    app: Mark set_var as unsafe
    It is unsafe nowadways.

--- podcasts-gtk/src/app.rs
+++ podcasts-gtk/src/app.rs
@@ -495,7 +495,7 @@ impl PdApplication {
         textdomain("gnome-podcasts").expect("Unable to switch to the text domain");
 
         // Make sure the app icon shows up in PulseAudio settings
-        env::set_var("PULSE_PROP_application.icon_name", APP_ID);
+        unsafe { env::set_var("PULSE_PROP_application.icon_name", APP_ID); }
 
         let application = Self::new();
 
